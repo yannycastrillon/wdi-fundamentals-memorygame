feaa8c529ded3a3e2cbc05c6e8dd6058476149bd
Yanny Castrillon <yannycastrillon@Yannys-MBP.attlocal.net>
2016-11-10 00:10:39 -0800

    Comparisons between cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,22 @@ var cardOne ="queen";
 var cardTwo ="queen";
 var cardThree ="king";
 var cardFour ="king";
+
+if(cardOne === cardTwo){
+	alert("You found a match Quens");
+}
+if (cardThree === cardFour){
+	alert("You found a match of Kings");
+}
+if (cardOne === cardThree){
+	alert("Not match found");
+}
+if (cardOne === cardFour){
+	alert("Not match found");
+}
+if (cardtwo === cardThree){
+	alert("Not match found");
+}
+if (cardtwo === cardFour){
+	alert("Not match found");
+}
